b81985d3ccfc1a4deb1405410a8f41f0111fb2a6
Luane Loureiro <luane.loureiro@esx.com.br>
2025-01-03 18:01:49 -0300

    Delete exercicio01 directory

--- exercicio01/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Exercicios.Ex01;
-
-Ex01 ex01 = new Ex01();
-
-int resultadoEx01 = ex01.SomaDoisNumeros(1, 1);
-
-Console.WriteLine($"A soma é {resultadoEx01}");
