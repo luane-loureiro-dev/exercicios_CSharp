ce9aa6a48ae3098a535328ac245a67d410bcf2a0
Luane Loureiro <luane.loureiro@esx.com.br>
2025-01-03 18:00:55 -0300

    Add files via upload

---
+++ logica/exercicio07/Program.cs
@@ -0,0 +1,23 @@
+﻿// Escreva um programa que leia um array de números inteiros e encontre o maior elemento.
+
+class MaiorNumero
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite os números separados por espaço: ");
+        string entrada = Console.ReadLine();
+
+        int[] numeros = entrada.Split(' ').Select(int.Parse).ToArray();
+
+        // Ordena em ordem crescente
+        Array.Sort(numeros);
+
+        // Pega o último elemento do array
+        int ultimoElemento = numeros[numeros.Length - 1];
+
+        // Exibe o último elemento
+        Console.WriteLine($"Maior Numero: {ultimoElemento}");
+        
+    }
+}
+
---
+++ logica/exercicio09/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class OrdemAlfabetica
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite os nomes separados por espaço: ");
+        string entrada = Console.ReadLine();
+        string[] nomes = entrada.Split(' ');
+
+        Array.Sort(nomes);
+
+        Console.WriteLine("Nomes em ordem alfabética:");
+        Console.WriteLine(string.Join(", ", nomes));
+        
+    }
+}
